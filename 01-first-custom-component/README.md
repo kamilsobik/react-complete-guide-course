a846d3f509f944617303b7445fe9d41a619af917
Kamil Sobik <kamil.sobik@gmail.com>
2023-02-14 22:16:35 +0100

    add first project

---
+++ 01-first-custom-component/README.md
@@ -0,0 +1,5 @@
+# react-complete-guide-code-part1
+
+## Udemy
+
+[Link do course](https://www.udemy.com/course/react-the-complete-guide-incl-redux/)
---
+++ 01-first-custom-component/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpeneseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseDate = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    props.onSaveExpenseData(expenseDate);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            onChange={amountChangeHandler}
+            value={enteredAmount}
+            min="0.01"
+            step="0.01"
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            onChange={dateChangeHandler}
+            value={enteredDate}
+            min="2023-02-01"
+            max="2024-12-31"
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpeneseForm;
